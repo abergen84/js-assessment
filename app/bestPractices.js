b7c06a422ff8dd8797e44c88c0014a48d790d2c9
Adam Bergen <bergen@gmail.com>
2015-01-06 20:16:29 -0600

    best practices complete

--- app/bestPractices.js
+++ app/bestPractices.js
@@ -1,4 +1,6 @@
-if (typeof define !== 'function') { var define = require('amdefine')(module); }
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
 
 /**
  * This file defines an object with some methods. Some of these methods are
@@ -6,31 +8,45 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
  * populated at all; your job is to fill them out.
  */
 define(function() {
-  return {
-    globals : function() {
-      myObject = {
-        name : 'Jory'
-      };
+    return {
+        globals: function() {
+          "use strict";
+            var myObject = {  //needed to declare the variable here with "var"
+                name: 'Jory'
+            };
 
-      return myObject;
-    },
+            return myObject;
+        },
 
-    functions : function(flag) {
-      if (flag) {
-        function getValue() { return 'a'; }
-      } else {
-        function getValue() { return 'b'; }
-      }
+        functions: function(flag) {
+          "use strict";
+            var getValue;
 
-      return getValue();
-    },
+            if (flag) {
+                getValue = function() {   //order of the function syntax was incorrect
+                    return 'a';
+                }
+            } else {
+                getValue = function() {
+                    return 'b';
+                }
+            }
 
-    parseInt : function(num) {
-      return parseInt(num);
-    },
+            return getValue();
+        },
 
-    identity : function(val1, val2) {
+        parseInt: function(num) {
+            "use strict";
+            return parseInt(num,10);
+        },
 
-    }
-  };
+        identity: function(val1, val2) {
+          "use strict";
+          if (val1 === val2){  //setup a simple if/else statement to return whether the 2 values match or not.
+            return true;
+          } else {
+            return false;
+          }
+        }
+    };
 });
