5f746b25fb57596d846a88df80adb9f7d0b03fef
Adam Bergen <bergen@gmail.com>
2015-01-13 22:49:40 -0600

    objects piece completed

--- app/objects.js
+++ app/objects.js
@@ -1,17 +1,25 @@
 if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
+  "use strict";
   return {
     alterContext : function(fn, obj) {
-
+    	obj.fn = fn;
+    	return obj.fn();
     },
 
     alterObjects : function(constructor, greeting) {
-
+    	constructor.prototype.greeting = greeting;
     },
 
     iterate : function(obj) {
-
+		var res = [];
+      	for (var prop in obj) {
+        if (obj.hasOwnProperty(prop)) {
+          res.push(prop + ": " + obj[prop]);
+        }
+      }
+      return res;
     }
   };
 });
