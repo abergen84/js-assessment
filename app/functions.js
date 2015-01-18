6ade100acd965bca4eedcde474c3f8d901c36ca4
Adam Bergen <bergen@gmail.com>
2015-01-17 19:46:43 -0600

    functions pretty much done

--- app/functions.js
+++ app/functions.js
@@ -17,23 +17,37 @@ define(function() {
     },
 
     makeClosures : function(arr, fn) {
-        
+        var res = [];
+        for (var i = 0; i < arr.length; i++){
+            res.push((function(num){
+                return function(){
+                    return fn(num);
+                }
+            })(arr[i]));
+        }
+        return res;
     },
 
     partial : function(fn, str1, str2) {
-
+        return function(x){
+            return fn(str1, str2, x);
+        }
     },
 
     useArguments : function() {
-
+        var sum = 0;
+        for (var i = 0; i < arguments.length; i++){
+            sum += arguments[i];
+        }
+            return sum;
     },
 
     callIt : function(fn) {
-
+        return fn.apply(null, Array.prototype.slice.call(arguments, 1));
     },
 
     partialUsingArguments : function(fn) {
-
+        
     },
 
     curryIt : function(fn) {
