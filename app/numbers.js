1b8946cf31eb3028c8b19a47b5c61eb8ae1e820e
Adam Bergen <bergen@gmail.com>
2015-01-19 15:05:20 -0600

    numbers pretty much done

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,25 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
+        var mask = 1 << (bit - 1);
 
+        return (num & mask) ? 1 : 0;
     },
 
     base10: function(str) {
-
+        return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+        var str = '';
+        for (var i = 0; i < 8; i++) {
+        str = ((num & Math.pow(2, i)) ? '1' : '0') + str;
+      }
+      return str;
     },
 
     multiply: function(a, b) {
-
+        
     }
   };
 });
