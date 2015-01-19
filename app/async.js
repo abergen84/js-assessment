b7352d78a38944d37b8d8578a30b32ae25bbb806
Adam Bergen <bergen@gmail.com>
2015-01-19 00:52:10 -0600

    whew, this assessment is no laughing matter..

--- app/async.js
+++ app/async.js
@@ -3,11 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
+    	var def = $.Deferred();
+
+    	setTimeout(function(){
+    		def.resolve(value);
+    	}, 10);
+
+    	return def.promise();
 
     },
 
     manipulateRemoteData : function(url) {
+    	var def = $.Deferred();
+    	var peopleInArray = [];
 
+    	$.getJSON(url).then(function(data){
+    		data['people'].forEach(function(x){
+    			peopleInArray.push(x.name);
+    		})
+    		def.resolve(peopleInArray.sort());
+    	})
+    	return def.promise();
     }
   };
 });
--- app/regex.js
+++ app/regex.js
@@ -3,26 +3,27 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     containsNumber : function(str) {
-
+        return /[0-9]/.test(str);
     },
 
     containsRepeatingLetter : function(str) {
-
+        return /([a-zA-Z])\1+/.test(str);
     },
 
     endsWithVowel : function(str) {
-
+        return /.*[aeiouAEIOU]$/.test(str);
     },
 
     captureThreeNumbers : function(str) {
-
+        var res = /([0-9]{3})/.exec(str);
+        return res ? res[0] : false;
     },
 
     matchesPattern : function(str) {
-
+        return /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(str);
     },
     isUSD : function(str) {
-
+        return /^\$[0-9]{1,3}(,[0-9]{3})*(\.[0-9]{2}){0,1}$/.test(str)
     }
   };
 });
